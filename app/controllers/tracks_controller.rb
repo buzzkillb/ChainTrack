afd7c29067c101075dcd8ecedd7b48c82529822c
Caleb Cohoon <cvl3b@fastmail.com>
2017-10-14 15:54:09 -0700

    Added tracks controller with routing

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,10 @@
+class TracksController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'tracks#new'
+
+  resources :tracks, only: [:new, :show, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
