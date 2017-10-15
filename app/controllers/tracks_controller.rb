6dfa2f1380b827739eab1715fd11774ab2484d9f
Caleb Cohoon <cvl3b@fastmail.com>
2017-10-15 10:51:28 -0700

    Track show page is now dynamic

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,16 +1,18 @@
 class TracksController < ApplicationController
   def new
     @track = Track.new
-    2.times { @track.wallets.build }
+    @track.wallets.build
   end
 
   def show
+    @track = Track.find_by_token params[:id]
+    redirect_to :root unless @track.present?
   end
 
   def create
     @track = Track.new(track_params)
     if @track.save
-      redirect_to @track
+      redirect_to "/#{@track.token}"
     else
       render action: 'new'
     end
