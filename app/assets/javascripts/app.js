1d317007628d3f6c51ef97ef96e1610caee8d9a3
Caleb Cohoon <cvl3b@fastmail.com>
2017-10-16 14:20:47 -0700

    Added ability to copy the wallet address to clipboard

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,30 @@
 $(function () {
+  var clipboard = new Clipboard('.clipboard-btn');
+  
+  var setTooltip = function (btn, message) {
+    $(btn).tooltip('hide')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  };
+
+  var hideTooltip = function (btn) {
+    setTimeout(function() {
+      $(btn).tooltip('dispose');
+    }, 1000);
+  };
+  
+  $('.clipboard-btn').tooltip({ trigger: 'click', placement: 'bottom' });
+  
+  clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+  });
+
+  clipboard.on('error', function(e) {
+    setTooltip(e.trigger, 'Failed!');
+    hideTooltip(e.trigger);
+  });
+  
   if($('#summary').length > 0) {
       var token = $('#summary').data('token');
       $.get('/summary/' + token, function (data) {
