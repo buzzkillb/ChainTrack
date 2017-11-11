4ef9d7b39528c89cb0d39c88e6deaa39c0555f22
Caleb Cohoon <cvl3b@fastmail.com>
2017-11-10 21:06:14 -0800

    Customized the admin pages

--- app/admin/track.rb
+++ app/admin/track.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Track do
   permit_params :currency_id, :token
+  
+  filter :wallets, collection: -> { Wallet.all.map { |w| [w.description, w.id] } }
+  filter :currency
+  filter :token
+  filter :created_at
+  filter :updated_at
+  
+  index do
+    id_column
+    column :token do |track|
+      link_to track.token, "/#{track.token}"
+    end
+    column :currency
+    column :created_at
+    column :updated_at
+    actions
+  end
+  
+  show do
+    attributes_table do
+      row :token do |track|
+        link_to track.token, "/#{track.token}"
+      end
+      row :currency
+      row :created_at
+      row :updated_at
+      active_admin_comments
+    end
+  end
 end
