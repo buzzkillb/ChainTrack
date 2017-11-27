63d528db4aa5cde6454ae72d1c669d3182e43784
Caleb Cohoon <cvl3b@fastmail.com>
2017-11-26 16:13:28 -0800

    Updated badge color ranges

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -2,8 +2,8 @@ module WalletsHelper
   def get_badge_details wallet
     days_since_updated = (Time.current.to_date - wallet.last_changed.to_date).to_i
     case days_since_updated
-      when 0 then { icon: 'success', text: 'Fresh' }
-      when 1..2 then { icon: 'warning', text: 'Aging' }
+      when 0..1 then { icon: 'success', text: 'Fresh' }
+      when 1..3 then { icon: 'warning', text: 'Aging' }
       else { icon: 'danger', text: 'Old' }
     end
   end
