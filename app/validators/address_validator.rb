52bb920d48ff28f1658f3fc8c883a7ac48f677cf
buzzkillb <travanx@gmail.com>
2018-03-10 18:43:47 -0800

    Update address_validator.rb

--- app/validators/address_validator.rb
+++ app/validators/address_validator.rb
@@ -1,6 +1,6 @@
 class AddressValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
-    if record.total_chc < 1000
+    if record.total_dnr < 5000
       record.errors[attribute] << 'does not seem to belong to a masternode'
     end
   end
