28c082c344ce8a4fab0c1b97d2f2161362aaf353
buzzkillb <travanx@gmail.com>
2018-03-10 18:42:33 -0800

    Update wallet_test.rb

--- test/models/wallet_test.rb
+++ test/models/wallet_test.rb
@@ -1,17 +1,17 @@
 require 'minitest/mock'
 
 class WalletTest < ActiveSupport::TestCase
-  test 'return 0 when less than 1000' do
+  test 'return 0 when less than 5000' do
     wallet = Wallet.new(address: 'address')
-    wallet.stub :total_chc, 1 do
-      assert_equal 0, wallet.chc_earned
+    wallet.stub :total_dnr, 1 do
+      assert_equal 0, wallet.dnr_earned
     end
   end
   
   test 'return earned amount' do
     wallet = Wallet.new(address: 'address')
-    wallet.stub :total_chc, 1035 do
-      assert_equal 35, wallet.chc_earned
+    wallet.stub :total_dnr, 1035 do
+      assert_equal 35, wallet.dnr_earned
     end
   end
 end
